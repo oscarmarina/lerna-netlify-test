fdbd265ce7f2df7edc52325df3949747908c13a5
oscar marina <oscar7600@outlook.com>
2026-03-12 07:11:37 +0100

    chore(component-b): update default heading

--- packages/components/component-b/test/component-b.test.ts
+++ packages/components/component-b/test/component-b.test.ts
@@ -25,9 +25,9 @@ describe('ComponentB', () => {
       fixtureCleanup();
     });
 
-    it('has a default heading "Hey there" and counter 5', () => {
+    it('has a default heading "Hey there B" and counter 5', () => {
       const button = elLocator.getByText('Counter: 5').query();
-      const heading = elLocator.getByText('Hey there').query();
+      const heading = elLocator.getByText('Hey there B').query();
       expect(button).toBeTruthy();
       expect(heading).toBeTruthy();
     });
