9392ec6acd09dd6db69d4c288b99d16e0351fb19
oscar marina <oscar7600@outlook.com>
2026-08-02 18:56:30 +0200

    chore(component-a): update component packages to support development mode and improve build process

---
+++ packages/components/component-a/dist/define/component-a.js
@@ -0,0 +1,4 @@
+import { t as ComponentA } from "../ComponentA-00uwVZA0.js";
+//#region src/define/component-a.ts
+window.customElements.define("component-a", ComponentA);
+//#endregion
---
+++ packages/components/component-a/dist/index.js
@@ -0,0 +1,2 @@
+import { t as ComponentA } from "./ComponentA-00uwVZA0.js";
+export { ComponentA };
