6de2f9b3a3858bbb77dccea2bb09078d5821144c
oscar marina <oscar7600@outlook.com>
2026-08-02 18:57:28 +0200

    chore(component-c): update component packages to support development mode and improve build process

---
+++ packages/components/component-c/dist/ComponentC.js
@@ -0,0 +1,2 @@
+import { t as ComponentC } from "./ComponentC-CUVHjAQM.js";
+export { ComponentC };
---
+++ packages/components/component-c/dist/define/component-c.js
@@ -0,0 +1,4 @@
+import { t as ComponentC } from "../ComponentC-CUVHjAQM.js";
+//#region src/define/component-c.ts
+window.customElements.define("component-c", ComponentC);
+//#endregion
