00097d784fd0e86c3c27adecc12675f06058bfb7
oscar marina <oscar7600@outlook.com>
2026-08-14 18:34:37 +0200

    chore: update eslint configuration and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,6 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import {defineConfig} from 'eslint/config';
+import createConfig from '@blockquote/eslint-config';
 
-export default [
-  {
-    ignores: [
-      '**/.idea',
-      '**/.tmp',
-      '**/.vscode',
-      '**/.wireit',
-      '**/_site',
-      '**/build',
-      '**/coverage',
-      '**/dev',
-      '**/dist',
-      '**/reports',
-      '**/storybook-static',
-      '**/*screenshots*',
-      '**/*snapshots*',
-      '**/*.config.*',
-      '**/*.d.ts',
-      '**/*.min.js',
-      '**/*.workspace.*',
-    ], },
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2023,
-        ...globals.mocha,
-      },
-    },
-  },
-  js.configs.recommended,
-  {
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'warn',
-    },
-  },
-];
+const eslintConfig = defineConfig(createConfig({tsconfigRootDir: import.meta.dirname}));
+
+export default  eslintConfig
